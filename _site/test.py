31a58bee1578d5035bfb25107548dc0af7354b51
LanceBasa <lance_olisco@yahoo.com>
2024-09-13 18:08:16 +0800

    setup directories and baseplates

---
+++ _site/test.py
@@ -0,0 +1,23 @@
+# def introduce(me):
+#     details = {
+#         name : "John",
+#     }
+    
+
+# my_info = introduce("John")
+
+my_info = {
+    "Name": "Lance Basa",
+    "Age": 25,
+    "City": "Perth",
+    "Country": "Australia",
+    "University": "University of Western Australia",
+    "degree": "Bachelor of Science",
+    "major": "Computer Science",
+    "major": "Cyber Security",
+    "minor": "Data Science",
+
+}
+
+for info in my_info:
+    print(info, my_info[info])
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// // script.js
+// document.addEventListener("DOMContentLoaded", () => {
+//     const line1 = document.querySelector('.line1');
+//     const line2 = document.querySelector('.line2');
+  
+//     // Duration of the first line typing animation in milliseconds
+//     const line1Duration = 5000; // Adjust according to the animation duration set in CSS
+  
+//     setTimeout(() => {
+//       line2.style.animation = 'typing 5s steps(40, end) forwards, blink-caret .75s step-end infinite';
+//     }, line1Duration); // Delay the start of the second line's animation
+//   });
+  
